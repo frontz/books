dd67a93e3cd4a095ccf730b359f1d5ca8e4984ec
Tomasz Fraczek <tom.frontz@gmail.com>
2023-02-23 08:50:03 +0100

    header and recipe model added

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is test description', 'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/VA56ZXQQMUI63CHIYWG4HW5O4I.jpg&w=860')
+  ];
 
   constructor() { }
 
